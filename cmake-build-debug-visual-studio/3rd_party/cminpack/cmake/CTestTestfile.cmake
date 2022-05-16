39f162cdf3a94779485a9bf709f9ba2f6337db04
YitongXia <xiayitong0630@gmail.com>
2022-05-16 19:41:44 +0200

    initial commit

---
+++ cmake-build-debug-visual-studio/3rd_party/cminpack/cmake/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/pro/Desktop/A2_Triangulation/A2_Triangulation_Code/3rd_party/cminpack/cmake
+# Build directory: C:/Users/pro/Desktop/A2_Triangulation/A2_Triangulation_Code/cmake-build-debug-visual-studio/3rd_party/cminpack/cmake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
